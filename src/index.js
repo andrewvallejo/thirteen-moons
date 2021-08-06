ca3712bdd631a408c296a053c4060a7f105e4b4a
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-05 18:15:34 -0600

    Add styles import

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './components/App';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import './styles/styles.scss'
 
 ReactDOM.render(
-  <Router>
+ <Router>
     <React.StrictMode>
       <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  </Router>
+    </React.StrictMode>
+  </Router>,
+  document.getElementById('root')
 );
+
 
