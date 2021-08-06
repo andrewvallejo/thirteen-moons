de5003b7e1182efda53048ddb232cab6d6d887b6
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-05 20:39:42 -0600

    Add import file for card covers to utility

---
+++ src/utility/cardCovers.js
@@ -0,0 +1,25 @@
+import cover1 from "../assets/celtic-gem.png";
+import cover2 from "../assets/diamond-grasp.png";
+import cover3 from "../assets/emerald-grasp.png";
+import cover4 from "../assets/eye-clasp.png";
+import cover5 from "../assets/eye-flower.png";
+import cover6 from "../assets/hand-gem-pandelum.png";
+import cover7 from "../assets/lunar-flower.png";
+import cover8 from "../assets/moon-pandelum.png";
+import cover9 from "../assets/saturn-pandelum.png";
+import cover10 from "../assets/shiny-gem.png";
+import cover11 from "../assets/sun-lunar-flower.png";
+
+export {
+  cover1,
+  cover2,
+  cover3,
+  cover4,
+  cover5,
+  cover6,
+  cover7,
+  cover8,
+  cover9,
+  cover10,
+  cover11,
+};
