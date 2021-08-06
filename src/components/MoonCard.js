902d48c515b46950609dfcad2dd94f88ed243a67
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-05 20:57:14 -0600

    Remove source img and replace with random cover

--- src/components/MoonCard.js
+++ src/components/MoonCard.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { nanoid } from 'nanoid'
+import { createCover } from '../utility/util'
 
 export const MoonCard = ({ deck }) => {
    return deck.map((card) => {
+
       const id = nanoid(10)
-      const { code, image, suit, value } = card
+      const { code, suit, value } = card
       return (
         <article className="moon-card" key={id}>
-         <img alt="card" src={image} id={code} key={id} value={value} className={suit}  />
+         <img alt="card" src={createCover()} id={code} key={id} value={value} className={suit}  />
         </article>
        )
    })
