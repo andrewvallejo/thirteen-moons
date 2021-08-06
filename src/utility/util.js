1a85e9c657f9306d131241bc2ee1c6ff125df780
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-06 12:53:35 -0600

    Add methods that shuffles, draws, and discards

--- src/utility/util.js
+++ src/utility/util.js
@@ -1,7 +1,17 @@
 import { covers } from "./cardCovers";
 
-export const randomize = (items) => {
-  return Math.floor(Math.random() * items.length);
-}
+export const randomize = (items) => Math.floor(Math.random() * items.length);
 
-export const createCover = () => covers[randomize(covers)]
+export const createCover = () => covers[randomize(covers)];
+
+export const shuffle = (deck) => deck.sort(() => Math.random() - 0.5)
+
+export const drawFour = (deck) => deck.splice(-4).map((card) => card)
+
+export const discard = (deck, cards) => {
+  return deck.filter((card) => {
+    const drawnCard = cards.map((card) => card)
+    const leftover = card !== drawnCard.abrv
+    return leftover
+  })
+}
