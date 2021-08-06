0b82255df62b24d397130cfaa96739120dcb3823
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-06 14:30:41 -0600

    Update reducer to have currentDeck method

--- src/utility/reducer.js
+++ src/utility/reducer.js
@@ -2,6 +2,8 @@ export const reducer = (state, action) => {
   switch(action.type) {
     case 'ADD_DECK':
       return {...state, deck: action.deck}
+    case 'UPDATE_DECK':
+      return {...state, deck: action.deck}
     default:
       return state
   }
@@ -11,7 +13,33 @@ export const getAction = (type, item) =>  {
   switch (type) {
     case 'deck':
       return ({type: 'ADD_DECK', deck: item})
+    case 'currentDeck':
+      return ({type: 'UPDATE_DECK', deck: item})
     default:
       break;
   }
 }  
+
+
+// export const reducer = (state, action) => {
+//   switch(action.type) {
+//     case 'ADD_DECK':
+//       return {...state, deck: action.deck}
+//     case 'UPDATE_DECK':
+//       return {deck: action.deck}
+//     default:
+//       return state
+//   }
+// }
+ 
+// export const getAction = (item) =>  {
+//   switch (item.toString()) {
+//     case 'cards':
+//       return ({type: 'ADD_DECK', deck: item})
+//     case 'deck':
+//       return ({type: 'UPDATE_DECK', deck: item})
+//     default:
+//       break;
+//   }
+// }  
+
