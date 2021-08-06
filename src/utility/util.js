51cc75aa773a47a6b978d63e6a792feceed6e609
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-05 20:56:46 -0600

    Add randomize for cover inside of util

--- src/utility/util.js
+++ src/utility/util.js
@@ -1,3 +1,7 @@
+import { covers } from "./cardCovers";
+
 export const randomize = (items) => {
   return Math.floor(Math.random() * items.length);
 }
+
+export const createCover = () => covers[randomize(covers)]
