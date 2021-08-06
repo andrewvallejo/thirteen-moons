216be182a89f6eb4d251711d08b653fd9b36c2d9
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-05 18:15:22 -0600

    Add foundation for App including connected API

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
+import React, { useEffect, useReducer  } from 'react';
+import { MoonCard } from './Card'
+import { reducer } from '../utility/reducer';
+import { fetchDeck } from '../utility/api'
 
-
-
- export const App = () => {
-  return (
-    <>
-    </>
-  );
+const initialState = {
+  deck: []
 }
 
+export const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    (async () => {
+      const cards = await fetchDeck()
+      const action = {type: 'ADD_DECK', deck: cards}  
+      dispatch(action)
+    })();
+  }, []);
+ 
+    return (
+       <main>
+          <MoonCard deck={state.deck} />
+      </main> 
+    )
+}
