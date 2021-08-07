d37af80d1fcae3ec50c1034149ae65094d167ac6
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-06 20:04:06 -0600

    Remove blending-modes div

--- src/components/MoonCard.js
+++ src/components/MoonCard.js
@@ -4,16 +4,13 @@ import { createCover } from '../utility/util'
 
 export const MoonCard = ({ deck }) => {
    return deck.map((card) => {
-
       const id = nanoid(10)
       const { code, suit, value } = card
       return (
          <article className='moon-card' key={id}>
             <div className="style-container"> 
                <img className="card-image" alt="card" src={createCover()} id={code} key={id} value={value} talent={suit} />
-              <div className="blending-modes">
                <aside className='darken'/> <aside className='overlay'/> <aside className='hue'/> 
-               </div>
             </div>
          </article>
        )
