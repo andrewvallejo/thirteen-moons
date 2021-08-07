d366d6833ec9466a992706871dc0888143edeeab
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-07 16:39:40 -0600

    Add MsgBar component

--- src/components/Spread.js
+++ src/components/Spread.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { MoonCard } from './MoonCard'
 import { MoonLevel } from "./MoonLevel"
+import { MoonMsgBar } from './MoonMsgBar'
 
 export const Spread = ({deck, hand, draw}) => {
     return (
     <section className="card-spread">
+      <MoonMsgBar />
       <MoonLevel levels={deck}/>
         <MoonCard deck={hand}/>
       </section>
