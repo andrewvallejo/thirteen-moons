28a1dff69c1fd2590a78290b270c82b7cf34e3f9
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-07 17:56:40 -0600

    Fix unused variable

--- src/components/MoonLevel.js
+++ src/components/MoonLevel.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Moon } from './Moon'
 import { nanoid } from 'nanoid'
 import  divider  from '../assets/lavender-divider.png'
 
 export const MoonLevel = () => {
   const id = nanoid(10)
-  const moons =  Array.from(Array(13), moon => <Moon />)
+  const moons =  Array.from(Array(13), () => <Moon />)
 
   
   return (
