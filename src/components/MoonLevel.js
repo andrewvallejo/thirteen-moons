85be1a4f9e4f1b669b690ce7fdf769ab45d148c9
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-07 18:56:57 -0600

    Add Link to test route param

--- src/components/MoonLevel.js
+++ src/components/MoonLevel.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useRouteMatch } from 'react-router-dom'
 import { Moon } from './Moon'
 import { nanoid } from 'nanoid'
 import  divider  from '../assets/lavender-divider.png'
 
 export const MoonLevel = () => {
+  const [level, setLevel] = useState(1)
   const id = nanoid(10)
-  const moons =  Array.from(Array(13), () => <Moon />)
+  const match = useRouteMatch().url;
+  const moons =  Array.from(Array(13), (moon, level) => <Moon id={level + 1} />)
 
-  
   return (
     <article className='moon-levels' key={id}>
+    <Link to={`${match}/${level}`} 
+    onClick={()=>setLevel(level+1)}
+    >
+    Click Me!</Link>
     <img alt="a lavender line" className="moon-divider"src={divider}/>
         {moons}
     </article>
