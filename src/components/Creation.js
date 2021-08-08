1eb01616b53f66708741b0a7e0b6e9ed7ebda501
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-08 15:04:10 -0600

    Refactor form so the the card updates on screen

--- src/components/Creation.js
+++ src/components/Creation.js
@@ -1,32 +1,70 @@
-/* eslint-disable no-unused-vars */
 import React, { useState } from "react"
+
+import { DummyCard } from "./DummyCard"
 import { MoonMsgBar } from "./MoonMsgBar"
 
 
 export const Creation = () =>  {
-  const [talent, setTalent] = useState('')
-  const [count, setCount] = useState(0)
-  const [terms, setTerms] = useState('')
+  const [code, setCode] = useState('') 
+  const [talent, setTalent] = useState('talent')
+  const [terms, setTerms] = useState('Find space and meditate for')
+  const [count, setCount] = useState('7')
+  const [interval, setInterval] = useState('minutes')
 
-  const onChange = (event) => {
-    event.preventDefault()
-    setTalent(event.target.value)
-    setCount(event.target.value)
-    setTerms(event.target.value)
+
+  const blankCard = {
+    code: '',
+    talent: talent ,
+    terms: terms ,
+    count: count,
+    interval: interval
   }
-
+  
   return (
-  <section className="creation-view">
-
-    <MoonMsgBar />
+    <section className="creation-view">
+      <DummyCard card={blankCard} />   
+      <MoonMsgBar  />
       <form className="creation-form">
-        <select value={talent} onChange={onChange}>
+        <label for="talent">Choose a talent</label> 
+        <select 
+          id='talent'
+          name='talent'
+          value={talent} 
+          onChange={(event) => 
+          setTalent(event.target.value)}>
           <option name="mind">Mind</option>
           <option name="spirit">Spirit</option>
           <option name="wisdom">Wisdom</option>
           <option name="Vitality">Vitality</option>
         </select>
-        
+        <label for='count'>Choose a number between 1 and 13.</label> 
+        <input 
+          id='count'
+          type="range" 
+          name="count"
+          min="1" max="13"
+          onChange={(event) => 
+          setCount(event.target.value)}/>
+        <label for='terms'>Write your terms.</label> 
+        <input 
+          id='terms'
+          type="text"
+          name="terms"
+          placeholder="Enter your challenge here"
+          onChange={(event) => 
+          setTerms(event.target.value)} />
+        <label for='interval'>Choose an interval.</label> 
+        <select 
+          id='interval'
+          value={interval} 
+          onChange={(event) => 
+          setInterval(event.target.value)}>
+          <option name="minutes">minutes</option>
+          <option name="repetition">repetitions</option>
+          <option name="counts">counts</option>
+          <option name="pages">pages</option>
+          <option name="chapter">chapters</option>
+        </select>
       </form>
     </section>
   )
