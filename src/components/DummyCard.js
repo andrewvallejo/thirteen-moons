7a7d8e818b75112c2824db270cb593598768513e
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-08 15:03:43 -0600

    Add a component for the CreationCard

---
+++ src/components/DummyCard.js
@@ -0,0 +1,33 @@
+import { nanoid } from 'nanoid'
+import React  from 'react'
+import { dummyCover } from '../utility/cardCovers'
+
+export const DummyCard = ({ card }) => {
+ const { code, talent, terms, count, interval } = card
+ const id = nanoid(10)
+    
+      return (
+         <article className='dummy-card' key={id}>
+               <div className="inner-card">
+                     <img className="card-back"    
+                        id={code} 
+                        key={id}
+                        alt="card" 
+                        src={dummyCover} />
+                     <aside className='darken'/> 
+                     <aside className='overlay'/>  
+                     <div className="card-front"> 
+                     <aside className="card-content">
+                        <h2 className='talent'>{talent}</h2>
+                        <h3 className='terms'>
+                           {terms}
+                              <span className="count">{count}</span>
+                           {interval}.
+                        </h3>
+                        <h3 className='talent mirrored'>{talent}</h3>
+                     </aside>
+                  </div>
+               </div>
+         </article>
+       )
+}
