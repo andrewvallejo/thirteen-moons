05163fb215c78e016e1f04f9a9c5fa43b0ed5f5c
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-08 09:20:06 -0600

    Remove route from Spread

--- src/components/Spread.js
+++ src/components/Spread.js
@@ -1,19 +1,15 @@
 import React from "react"
-import { useRouteMatch, Route } from 'react-router-dom';
+// import { useRouteMatch, Route } from 'react-router-dom';
 import { MoonCard } from './MoonCard'
 import { MoonLevel } from "./MoonLevel"
 import { MoonMsgBar } from './MoonMsgBar'
 
 export const Spread = ({deck, hand, draw}) => {
-  const match = useRouteMatch().url;
-
     return (
-      <Route exact path={`${match}/quarum/:level`}>
         <section className="card-spread">
               <MoonMsgBar />
               <MoonLevel levels={deck}/>
               <MoonCard deck={hand}/>
             </section>
-        </Route>
   )
 }
