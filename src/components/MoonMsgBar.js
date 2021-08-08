520d9041d08cbbcc1b9b0f999bffa51e206d9105
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-08 15:55:49 -0600

    Add dialog based on route up to level 2

--- src/components/MoonMsgBar.js
+++ src/components/MoonMsgBar.js
@@ -1,23 +1,30 @@
-import React from "react";
+import React from 'react';
 import moonFace from '../assets/moon-face.png'
 import { Route , Switch } from 'react-router-dom';
-import { nanoid } from "nanoid";
+import { nanoid } from 'nanoid';
 
 export const MoonMsgBar = () => {
   const id = nanoid(10)
+
   return (
     <Switch> 
       <>
-        <article className="message-bar" key={id}>
-            <img alt="the face of the moon" src={moonFace} className='moon-face' />
-              <blockquote className="dialog-container">
+        <article className='message-bar' key={id}>
+            <img alt='the face of the moon' src={moonFace} className='moon-face' />
+              <blockquote className='dialog-container'>
                 <Route  path='/wrong-turn'>
                   <p className= 'dialog'>Where do you think you are?</p>
                 </Route>
-                <Route exact path="/lunares/quarum/:level">
+                <Route exact path='/lunares/quarum/'>
                   <p className='dialog'>Choose your fate, child</p>
                 </Route>
-                <Route exact path="/">
+                <Route exact path='/lunares/quarum/1'>
+                  <p className='dialog'>Excellent child, you've found my first moon</p>
+                </Route>
+                <Route exact path='/lunares/quarum/2'>
+                  <p className='dialog'>Good, keep on going.</p>
+                </Route>
+                <Route exact path='/'>
                   <p className='dialog'>Craft a card?</p>
                 </Route>
             </blockquote>
