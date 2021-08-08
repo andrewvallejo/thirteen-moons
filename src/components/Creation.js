41f4686821645cbe63cb771b65d41ec595ce6940
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-08 08:48:07 -0600

    Add the talent aspect of the form

--- src/components/Creation.js
+++ src/components/Creation.js
@@ -8,11 +8,25 @@ export const Creation = () =>  {
   const [count, setCount] = useState(0)
   const [terms, setTerms] = useState('')
 
+  const onChange = (event) => {
+    event.preventDefault()
+    setTalent(event.target.value)
+    setCount(event.target.value)
+    setTerms(event.target.value)
+  }
+
   return (
   <section className="creation-view">
-    <MoonMsgBar />
-      <form>
 
+    <MoonMsgBar />
+      <form className="creation-form">
+        <select value={talent} onChange={onChange}>
+          <option name="mind">Mind</option>
+          <option name="spirit">Spirit</option>
+          <option name="wisdom">Wisdom</option>
+          <option name="Vitality">Vitality</option>
+        </select>
+        
       </form>
     </section>
   )
