1b38358d2792ad07dff57202e4d7e7f00568672d
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-07 19:53:25 -0600

    Remove everything that made MoonLevel cool

--- src/components/MoonLevel.js
+++ src/components/MoonLevel.js
@@ -1,23 +1,16 @@
-import React, { useState } from 'react'
-import { Link, useRouteMatch } from 'react-router-dom'
+import React from 'react'
 import { Moon } from './Moon'
 import { nanoid } from 'nanoid'
 import  divider  from '../assets/lavender-divider.png'
 
 export const MoonLevel = () => {
-  const [level, setLevel] = useState(1)
   const id = nanoid(10)
-  const match = useRouteMatch().url;
   const moons =  Array.from(Array(13), (moon, level) => <Moon id={level} key={level} />)
 
   return (
     <article className='moon-levels' key={id}>
-    <Link to={`${match}/${level}`} 
-    onClick={()=> setLevel(level + 1)}
-    >
-    Click Me!</Link>
-    <img alt="a lavender line" className="moon-divider"src={divider} />
-        {moons}
+      <img alt="a lavender line" className="moon-divider"src={divider} />
+          {moons}
     </article>
     )
 }
