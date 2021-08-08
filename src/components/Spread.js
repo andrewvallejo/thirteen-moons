5e9feb8b0e24bd348cfdee9b35d6d7eaf1c2d264
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-07 18:57:30 -0600

    Add Route to determine level

--- src/components/Spread.js
+++ src/components/Spread.js
@@ -1,14 +1,18 @@
 import React from "react"
+import { useRouteMatch, Route } from 'react-router-dom';
 import { MoonCard } from './MoonCard'
 import { MoonLevel } from "./MoonLevel"
 import { MoonMsgBar } from './MoonMsgBar'
 
 export const Spread = ({deck, hand, draw}) => {
+  const match = useRouteMatch().url;
     return (
-    <section className="card-spread">
-      <MoonMsgBar />
-      <MoonLevel levels={deck}/>
-        <MoonCard deck={hand}/>
-      </section>
+      <Route path={`${match}`} >
+        <section className="card-spread">
+              <MoonMsgBar />
+              <MoonLevel levels={deck}/>
+              <MoonCard deck={hand}/>
+            </section>
+        </Route>
   )
 }
