75109ba1254786ee9deebc338fa6794205ea7cdc
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-07 22:29:03 -0600

    Add different messages depending on url

--- src/components/MoonMsgBar.js
+++ src/components/MoonMsgBar.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from "react";
 import moonFace from '../assets/moon-face.png'
+import { Route , Switch } from 'react-router-dom';
 
 export const MoonMsgBar = () => {
   return (
-    <article className="message-bar">
-      <img alt="the face of the moon" src={moonFace} className='moon-face' />
-      <blockquote className="dialog-container">
-        <p className='dialog'>Choose your fate, Child.</p>
-      </blockquote>
-    </article>
+    <Switch>
+        <article className="message-bar">
+          <img alt="the face of the moon" src={moonFace} className='moon-face' />
+          <blockquote className="dialog-container">
+          <Route  path='/wrong-turn'>
+            <p className= 'dialog'>Where do you think you are?</p>
+          </Route>
+          <Route exact path="/lunares/quarum/:level">
+            <p className='dialog'>Choose your fate, child</p>
+          </Route>
+          <Route exact path="/">
+            <p className='dialog'>Craft a card?</p>
+          </Route>
+        </blockquote>
+      </article>
+    </Switch>
   )
 }
