26b3e0baca5240b4d9b50edc30ec5581b9334060
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-09 09:35:23 -0600

    Update interval to intervals due to reserved words

--- src/dialogData.js
+++ src/dialogData.js
@@ -4,362 +4,362 @@ export const dialogData = [
     talent: 'wisdom',
     terms: 'dapibus rutrum, justo. Praesent luctus. Curabitur egestas nunc sed libero.',
     count: 1,
-    interval: 'chapters'
+    intervals: 'chapters'
   },
   {
     code: 'w2',
     talent: 'wisdom',
     terms: 'sit amet, consectetuer adipiscing elit. Aliquam auctor, velit eget laoreet',
     count: 2,
-    interval: 'repetition'
+    intervals: 'repetition'
   },
   {
     code: 'w3',
     talent: 'wisdom',
     terms: 'ut aliquam iaculis, lacus pede sagittis augue, eu tempor erat',
     count: 3,
-    interval: 'chapters'
+    intervals: 'chapters'
   },
   {
     code: 'w4',
     talent: 'wisdom',
     terms: 'Vivamus molestie dapibus ligula. Aliquam erat volutpat. Nulla dignissim. Maecenas',
     count: 4,
-    interval: 'pages'
+    intervals: 'pages'
   },
   {
     code: 'w5',
     talent: 'wisdom',
     terms: 'amet metus. Aliquam erat volutpat. Nulla facilisis. Suspendisse commodo tincidunt',
     count: 5,
-    interval: 'counts'
+    intervals: 'counts'
   },
   {
     code: 'w6',
     talent: 'wisdom',
     terms: 'vel lectus. Cum sociis natoque penatibus et magnis dis parturient',
     count: 6,
-    interval: 'counts'
+    intervals: 'counts'
   },
   {
     code: 'w7',
     talent: 'wisdom',
     terms: 'augue id ante dictum cursus. Nunc mauris elit, dictum eu,',
     count: 7,
-    interval: 'pages'
+    intervals: 'pages'
   },
   {
     code: 'w8',
     talent: 'wisdom',
     terms: 'luctus aliquet odio. Etiam ligula tortor, dictum eu, placerat eget,',
     count: 8,
-    interval: 'minutes'
+    intervals: 'minutes'
   },
   {
     code: 'w9',
     talent: 'wisdom',
     terms: 'fermentum vel, mauris. Integer sem elit, pharetra ut, pharetra sed,',
     count: 9,
-    interval: 'minutes'
+    intervals: 'minutes'
   },
   {
     code: 'w10',
     talent: 'wisdom',
     terms: 'Etiam imperdiet dictum magna. Ut tincidunt orci quis lectus. Nullam',
     count: 10,
-    interval: 'repetition'
+    intervals: 'repetition'
   },
   {
     code: 'w11',
     talent: 'wisdom',
     terms: 'mi lorem, vehicula et, rutrum eu, ultrices sit amet, risus.',
     count: 11,
-    interval: 'minutes'
+    intervals: 'minutes'
   },
   {
     code: 'w12',
     talent: 'wisdom',
     terms: 'enim non nisi. Aenean eget metus. In nec orci. Donec',
     count: 12,
-    interval: 'chapters'
+    intervals: 'chapters'
   },
   {
     code: 'w13',
     talent: 'wisdom',
     terms: 'per inceptos hymenaeos. Mauris ut quam vel sapien imperdiet ornare.',
     count: 13,
-    interval: 'repetition'
+    intervals: 'repetition'
   },
   {
     code: 's1',
     talent: 'spirit',
     terms: 'dapibus rutrum, justo. Praesent luctus. Curabitur egestas nunc sed libero.',
     count: 1,
-    interval: 'chapters'
+    intervals: 'chapters'
   },
   {
     code: 's2',
     talent: 'spirit',
     terms: 'sit amet, consectetuer adipiscing elit. Aliquam auctor, velit eget laoreet',
     count: 2,
-    interval: 'repetition'
+    intervals: 'repetition'
   },
   {
     code: 's3',
     talent: 'spirit',
     terms: 'ut aliquam iaculis, lacus pede sagittis augue, eu tempor erat',
     count: 3,
-    interval: 'chapters'
+    intervals: 'chapters'
   },
   {
     code: 's4',
     talent: 'spirit',
     terms: 'Vivamus molestie dapibus ligula. Aliquam erat volutpat. Nulla dignissim. Maecenas',
     count: 4,
-    interval: 'pages'
+    intervals: 'pages'
   },
   {
     code: 's5',
     talent: 'spirit',
     terms: 'amet metus. Aliquam erat volutpat. Nulla facilisis. Suspendisse commodo tincidunt',
     count: 5,
-    interval: 'counts'
+    intervals: 'counts'
   },
   {
     code: 's6',
     talent: 'spirit',
     terms: 'vel lectus. Cum sociis natoque penatibus et magnis dis parturient',
     count: 6,
-    interval: 'counts'
+    intervals: 'counts'
   },
   {
     code: 's7',
     talent: 'spirit',
     terms: 'augue id ante dictum cursus. Nunc mauris elit, dictum eu,',
     count: 7,
-    interval: 'pages'
+    intervals: 'pages'
   },
   {
     code: 's8',
     talent: 'spirit',
     terms: 'luctus aliquet odio. Etiam ligula tortor, dictum eu, placerat eget,',
     count: 8,
-    interval: 'minutes'
+    intervals: 'minutes'
   },
   {
     code: 's9',
     talent: 'spirit',
     terms: 'fermentum vel, mauris. Integer sem elit, pharetra ut, pharetra sed,',
     count: 9,
-    interval: 'minutes'
+    intervals: 'minutes'
   },
   {
     code: 's10',
     talent: 'spirit',
     terms: 'Etiam imperdiet dictum magna. Ut tincidunt orci quis lectus. Nullam',
     count: 10,
-    interval: 'repetition'
+    intervals: 'repetition'
   },
   {
     code: 's11',
     talent: 'spirit',
     terms: 'mi lorem, vehicula et, rutrum eu, ultrices sit amet, risus.',
     count: 11,
-    interval: 'minutes'
+    intervals: 'minutes'
   },
   {
     code: 's12',
     talent: 'spirit',
     terms: 'enim non nisi. Aenean eget metus. In nec orci. Donec',
     count: 12,
-    interval: 'chapters'
+    intervals: 'chapters'
   },
   {
     code: 's13',
     talent: 'spirit',
     terms: 'per inceptos hymenaeos. Mauris ut quam vel sapien imperdiet ornare.',
     count: 13,
-    interval: 'repetition'
+    intervals: 'repetition'
   },
   {
     code: 'v1',
     talent: 'Vitality',
     terms: 'dapibus rutrum, justo. Praesent luctus. Curabitur egestas nunc sed libero.',
     count: 1,
-    interval: 'chapters'
+    intervals: 'chapters'
   },
   {
     code: 'v2',
     talent: 'Vitality',
     terms: 'sit amet, consectetuer adipiscing elit. Aliquam auctor, velit eget laoreet',
     count: 2,
-    interval: 'repetition'
+    intervals: 'repetition'
   },
   {
     code: 'v3',
     talent: 'Vitality',
     terms: 'ut aliquam iaculis, lacus pede sagittis augue, eu tempor erat',
     count: 3,
-    interval: 'chapters'
+    intervals: 'chapters'
   },
   {
     code: 'v4',
     talent: 'Vitality',
     terms: 'Vivamus molestie dapibus ligula. Aliquam erat volutpat. Nulla dignissim. Maecenas',
     count: 4,
-    interval: 'pages'
+    intervals: 'pages'
   },
   {
     code: 'v5',
     talent: 'Vitality',
     terms: 'amet metus. Aliquam erat volutpat. Nulla facilisis. Suspendisse commodo tincidunt',
     count: 5,
-    interval: 'counts'
+    intervals: 'counts'
   },
   {
     code: 'v6',
     talent: 'Vitality',
     terms: 'vel lectus. Cum sociis natoque penatibus et magnis dis parturient',
     count: 6,
-    interval: 'counts'
+    intervals: 'counts'
   },
   {
     code: 'v7',
     talent: 'Vitality',
     terms: 'augue id ante dictum cursus. Nunc mauris elit, dictum eu,',
     count: 7,
-    interval: 'pages'
+    intervals: 'pages'
   },
   {
     code: 'v8',
     talent: 'Vitality',
     terms: 'luctus aliquet odio. Etiam ligula tortor, dictum eu, placerat eget,',
     count: 8,
-    interval: 'minutes'
+    intervals: 'minutes'
   },
   {
     code: 'v9',
     talent: 'Vitality',
     terms: 'fermentum vel, mauris. Integer sem elit, pharetra ut, pharetra sed,',
     count: 9,
-    interval: 'minutes'
+    intervals: 'minutes'
   },
   {
     code: 'v10',
     talent: 'Vitality',
     terms: 'Etiam imperdiet dictum magna. Ut tincidunt orci quis lectus. Nullam',
     count: 10,
-    interval: 'repetition'
+    intervals: 'repetition'
   },
   {
     code: 'v11',
     talent: 'Vitality',
     terms: 'mi lorem, vehicula et, rutrum eu, ultrices sit amet, risus.',
     count: 11,
-    interval: 'minutes'
+    intervals: 'minutes'
   },
   {
     code: 'v12',
     talent: 'Vitality',
     terms: 'enim non nisi. Aenean eget metus. In nec orci. Donec',
     count: 12,
-    interval: 'chapters'
+    intervals: 'chapters'
   },
   {
     code: 'v13',
     talent: 'Vitality',
     terms: 'per inceptos hymenaeos. Mauris ut quam vel sapien imperdiet ornare.',
     count: 13,
-    interval: 'repetition'
+    intervals: 'repetition'
   },  {
     code: 'm1',
     talent: 'Mind',
     terms: 'dapibus rutrum, justo. Praesent luctus. Curabitur egestas nunc sed libero.',
     count: 1,
-    interval: 'chapters'
+    intervals: 'chapters'
   },
   {
     code: 'm2',
     talent: 'Mind',
     terms: 'sit amet, consectetuer adipiscing elit. Aliquam auctor, velit eget laoreet',
     count: 2,
-    interval: 'repetition'
+    intervals: 'repetition'
   },
   {
     code: 'm3',
     talent: 'Mind',
     terms: 'ut aliquam iaculis, lacus pede sagittis augue, eu tempor erat',
     count: 3,
-    interval: 'chapters'
+    intervals: 'chapters'
   },
   {
     code: 'm4',
     talent: 'Mind',
     terms: 'Vivamus molestie dapibus ligula. Aliquam erat volutpat. Nulla dignissim. Maecenas',
     count: 4,
-    interval: 'pages'
+    intervals: 'pages'
   },
   {
     code: 'm5',
     talent: 'Mind',
     terms: 'amet metus. Aliquam erat volutpat. Nulla facilisis. Suspendisse commodo tincidunt',
     count: 5,
-    interval: 'counts'
+    intervals: 'counts'
   },
   {
     code: 'm6',
     talent: 'Mind',
     terms: 'vel lectus. Cum sociis natoque penatibus et magnis dis parturient',
     count: 6,
-    interval: 'counts'
+    intervals: 'counts'
   },
   {
     code: 'm7',
     talent: 'Mind',
     terms: 'augue id ante dictum cursus. Nunc mauris elit, dictum eu,',
     count: 7,
-    interval: 'pages'
+    intervals: 'pages'
   },
   {
     code: 'm8',
     talent: 'Mind',
     terms: 'luctus aliquet odio. Etiam ligula tortor, dictum eu, placerat eget,',
     count: 8,
-    interval: 'minutes'
+    intervals: 'minutes'
   },
   {
     code: 'm9',
     talent: 'Mind',
     terms: 'fermentum vel, mauris. Integer sem elit, pharetra ut, pharetra sed,',
     count: 9,
-    interval: 'minutes'
+    intervals: 'minutes'
   },
   {
     code: 'm10',
     talent: 'Mind',
     terms: 'Etiam imperdiet dictum magna. Ut tincidunt orci quis lectus. Nullam',
     count: 10,
-    interval: 'repetition'
+    intervals: 'repetition'
   },
   {
     code: 'm11',
     talent: 'Mind',
     terms: 'mi lorem, vehicula et, rutrum eu, ultrices sit amet, risus.',
     count: 11,
-    interval: 'minutes'
+    intervals: 'minutes'
   },
   {
     code: 'm12',
     talent: 'Mind',
     terms: 'enim non nisi. Aenean eget metus. In nec orci. Donec',
     count: 12,
-    interval: 'chapters'
+    intervals: 'chapters'
   },
   {
     code: 'm13',
     talent: 'Mind',
     terms: 'per inceptos hymenaeos. Mauris ut quam vel sapien imperdiet ornare.',
     count: 13,
-    interval: 'repetition'
+    intervals: 'repetition'
   },
 ]
