0565b714e4e2a99eeeb851980a9f0613980c0cce
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-09 19:32:18 -0600

    Update information for each card

--- src/dialogData.js
+++ src/dialogData.js
@@ -2,364 +2,366 @@ export const dialogData = [
   {
     code: 'w1',
     talent: 'wisdom',
-    terms: 'dapibus rutrum, justo. Praesent luctus. Curabitur egestas nunc sed libero.',
+    terms: 'Read a spiritual book for',
     count: 1,
     intervals: 'chapters'
   },
   {
     code: 'w2',
     talent: 'wisdom',
-    terms: 'sit amet, consectetuer adipiscing elit. Aliquam auctor, velit eget laoreet',
+    terms: 'Ask someone older than you and take down',
     count: 2,
-    intervals: 'repetition'
+    intervals: 'nots'
   },
   {
     code: 'w3',
     talent: 'wisdom',
-    terms: 'ut aliquam iaculis, lacus pede sagittis augue, eu tempor erat',
+    terms: 'Read health and wellness',
     count: 3,
-    intervals: 'chapters'
+    intervals: 'articles'
   },
   {
     code: 'w4',
     talent: 'wisdom',
-    terms: 'Vivamus molestie dapibus ligula. Aliquam erat volutpat. Nulla dignissim. Maecenas',
+    terms: 'Call someone you love and talk to them for at least',
     count: 4,
-    intervals: 'pages'
+    intervals: 'minutes'
   },
   {
     code: 'w5',
     talent: 'wisdom',
-    terms: 'amet metus. Aliquam erat volutpat. Nulla facilisis. Suspendisse commodo tincidunt',
+    terms: 'Google', 
     count: 5,
-    intervals: 'counts'
+    intervals: 'interests'
   },
   {
     code: 'w6',
     talent: 'wisdom',
-    terms: 'vel lectus. Cum sociis natoque penatibus et magnis dis parturient',
+    terms: 'Engage philosophy, google someone for',
     count: 6,
-    intervals: 'counts'
+    intervals: 'minutes'
   },
   {
     code: 'w7',
     talent: 'wisdom',
-    terms: 'augue id ante dictum cursus. Nunc mauris elit, dictum eu,',
+    terms: 'Enjoy scenary, stare through the window for',
     count: 7,
-    intervals: 'pages'
+    intervals: 'minutes'
   },
   {
     code: 'w8',
     talent: 'wisdom',
-    terms: 'luctus aliquet odio. Etiam ligula tortor, dictum eu, placerat eget,',
+    terms: 'self-reflect, look at a mirror for',
     count: 8,
     intervals: 'minutes'
   },
   {
     code: 'w9',
     talent: 'wisdom',
-    terms: 'fermentum vel, mauris. Integer sem elit, pharetra ut, pharetra sed,',
+    terms: 'find a self-care book and read ',
     count: 9,
-    intervals: 'minutes'
+    Intervals: 'Pages'
   },
   {
     code: 'w10',
     talent: 'wisdom',
-    terms: 'Etiam imperdiet dictum magna. Ut tincidunt orci quis lectus. Nullam',
+    terms: 'write about',
     count: 10,
-    intervals: 'repetition'
+    intervals: 'things'
   },
   {
     code: 'w11',
     talent: 'wisdom',
-    terms: 'mi lorem, vehicula et, rutrum eu, ultrices sit amet, risus.',
+    terms: 'write about your day has affected you',
     count: 11,
     intervals: 'minutes'
   },
   {
     code: 'w12',
     talent: 'wisdom',
-    terms: 'enim non nisi. Aenean eget metus. In nec orci. Donec',
+    terms: 'Write different things you love for ',
     count: 12,
-    intervals: 'chapters'
+    intervals: 'counts'
   },
   {
     code: 'w13',
     talent: 'wisdom',
-    terms: 'per inceptos hymenaeos. Mauris ut quam vel sapien imperdiet ornare.',
+    terms: 'Incite wisdom to a friend, talk to them for',
     count: 13,
-    intervals: 'repetition'
+    intervals: 'meditate'
   },
   {
     code: 's1',
     talent: 'spirit',
-    terms: 'dapibus rutrum, justo. Praesent luctus. Curabitur egestas nunc sed libero.',
+    terms: 'Tell someone them how much you care about them and what they have done to improve your life for',
     count: 1,
-    intervals: 'chapters'
+    intervals: 'phonecall'
   },
   {
     code: 's2',
     talent: 'spirit',
-    terms: 'sit amet, consectetuer adipiscing elit. Aliquam auctor, velit eget laoreet',
+    terms: 'pray',
     count: 2,
-    intervals: 'repetition'
+    intervals: 'minutes'
   },
   {
     code: 's3',
     talent: 'spirit',
-    terms: 'ut aliquam iaculis, lacus pede sagittis augue, eu tempor erat',
+    terms: 'Find some space and meditate for',
     count: 3,
-    intervals: 'chapters'
+    intervals: 'minutes'
   },
   {
     code: 's4',
     talent: 'spirit',
-    terms: 'Vivamus molestie dapibus ligula. Aliquam erat volutpat. Nulla dignissim. Maecenas',
+    terms: 'Read about a spiritual article',
     count: 4,
-    intervals: 'pages'
+    intervals: 'minutes'
   },
   {
     code: 's5',
     talent: 'spirit',
-    terms: 'amet metus. Aliquam erat volutpat. Nulla facilisis. Suspendisse commodo tincidunt',
+    terms: 'pray for',
     count: 5,
-    intervals: 'counts'
+    intervals: 'minutes'
   },
   {
     code: 's6',
     talent: 'spirit',
-    terms: 'vel lectus. Cum sociis natoque penatibus et magnis dis parturient',
+    terms: 'write down good things that have happened to you today',
     count: 6,
-    intervals: 'counts'
+    intervals: 'items'
   },
   {
     code: 's7',
     talent: 'spirit',
-    terms: 'augue id ante dictum cursus. Nunc mauris elit, dictum eu,',
+    terms: 'Meditate for',
     count: 7,
     intervals: 'pages'
   },
   {
     code: 's8',
     talent: 'spirit',
-    terms: 'luctus aliquet odio. Etiam ligula tortor, dictum eu, placerat eget,',
+    terms: 'Count your blessings and write them for',
     count: 8,
     intervals: 'minutes'
   },
   {
     code: 's9',
     talent: 'spirit',
-    terms: 'fermentum vel, mauris. Integer sem elit, pharetra ut, pharetra sed,',
+    terms: 'Light a candle and stare at it for',
     count: 9,
     intervals: 'minutes'
   },
   {
     code: 's10',
     talent: 'spirit',
-    terms: 'Etiam imperdiet dictum magna. Ut tincidunt orci quis lectus. Nullam',
+    terms: 'List what you are grateful for',
     count: 10,
-    intervals: 'repetition'
+    intervals: 'nouns'
   },
   {
     code: 's11',
     talent: 'spirit',
-    terms: 'mi lorem, vehicula et, rutrum eu, ultrices sit amet, risus.',
+    terms: 'Write to the world for',
     count: 11,
     intervals: 'minutes'
   },
   {
     code: 's12',
     talent: 'spirit',
-    terms: 'enim non nisi. Aenean eget metus. In nec orci. Donec',
+    terms: 'do guided meditation for',
     count: 12,
     intervals: 'chapters'
   },
   {
     code: 's13',
     talent: 'spirit',
-    terms: 'per inceptos hymenaeos. Mauris ut quam vel sapien imperdiet ornare.',
+    terms: 'do 5 downward dogs for',
     count: 13,
-    intervals: 'repetition'
+    intervals: 'seconds each'
   },
   {
     code: 'v1',
     talent: 'Vitality',
-    terms: 'dapibus rutrum, justo. Praesent luctus. Curabitur egestas nunc sed libero.',
+    terms: 'Pace the Earth, Run for',
     count: 1,
-    intervals: 'chapters'
+    intervals: 'Miles'
   },
   {
     code: 'v2',
     talent: 'Vitality',
-    terms: 'sit amet, consectetuer adipiscing elit. Aliquam auctor, velit eget laoreet',
+    terms: 'Do pushups for a total of ',
     count: 2,
-    intervals: 'repetition'
+    intervals: 'minutes'
   },
   {
     code: 'v3',
     talent: 'Vitality',
-    terms: 'ut aliquam iaculis, lacus pede sagittis augue, eu tempor erat',
+    terms: 'Hug the floor, do planks for',
     count: 3,
-    intervals: 'chapters'
+    intervals: 'minutes'
   },
   {
     code: 'v4',
     talent: 'Vitality',
-    terms: 'Vivamus molestie dapibus ligula. Aliquam erat volutpat. Nulla dignissim. Maecenas',
+    terms: 'Do sit ups for',
     count: 4,
-    intervals: 'pages'
+    intervals: 'Reps of 10'
   },
   {
     code: 'v5',
     talent: 'Vitality',
-    terms: 'amet metus. Aliquam erat volutpat. Nulla facilisis. Suspendisse commodo tincidunt',
+    terms: 'Jump rope',
     count: 5,
-    intervals: 'counts'
+    intervals: 'Reps of 20'
   },
   {
     code: 'v6',
     talent: 'Vitality',
-    terms: 'vel lectus. Cum sociis natoque penatibus et magnis dis parturient',
+    terms: 'Do burpees',
     count: 6,
-    intervals: 'counts'
+    intervals: 'Reps of 5'
   },
   {
     code: 'v7',
     talent: 'Vitality',
-    terms: 'augue id ante dictum cursus. Nunc mauris elit, dictum eu,',
+    terms: 'Do cat and cow stretch',
     count: 7,
-    intervals: 'pages'
+    intervals: 'times'
   },
   {
     code: 'v8',
     talent: 'Vitality',
-    terms: 'luctus aliquet odio. Etiam ligula tortor, dictum eu, placerat eget,',
+    terms: 'Drink glass of water',
     count: 8,
-    intervals: 'minutes'
+    intervals: 'ounces'
   },
   {
     code: 'v9',
     talent: 'Vitality',
-    terms: 'fermentum vel, mauris. Integer sem elit, pharetra ut, pharetra sed,',
+    terms: 'Do squats',
     count: 9,
-    intervals: 'minutes'
+    intervals: 'Reps of 5'
   },
   {
     code: 'v10',
     talent: 'Vitality',
-    terms: 'Etiam imperdiet dictum magna. Ut tincidunt orci quis lectus. Nullam',
+    terms: 'Stretch',
     count: 10,
-    intervals: 'repetition'
+    intervals: 'Minutes'
   },
   {
     code: 'v11',
     talent: 'Vitality',
-    terms: 'mi lorem, vehicula et, rutrum eu, ultrices sit amet, risus.',
+    terms: 'Touch your toes',
     count: 11,
-    intervals: 'minutes'
+    intervals: 'Times'
   },
   {
     code: 'v12',
     talent: 'Vitality',
-    terms: 'enim non nisi. Aenean eget metus. In nec orci. Donec',
+    terms: 'Do jump squats for',
     count: 12,
-    intervals: 'chapters'
+    intervals: 'Reps of 2'
   },
   {
     code: 'v13',
     talent: 'Vitality',
-    terms: 'per inceptos hymenaeos. Mauris ut quam vel sapien imperdiet ornare.',
+    terms: 'Do slow calf raises for',
     count: 13,
-    intervals: 'repetition'
+    intervals: 'Sets of 2'
   },  {
     code: 'm1',
     talent: 'Mind',
-    terms: 'dapibus rutrum, justo. Praesent luctus. Curabitur egestas nunc sed libero.',
+    terms: 'Learn something new and read ',
     count: 1,
-    intervals: 'chapters'
+    intervals: 'article'
   },
   {
     code: 'm2',
     talent: 'Mind',
-    terms: 'sit amet, consectetuer adipiscing elit. Aliquam auctor, velit eget laoreet',
+    terms: 'Learn something new about your hobby and read',
     count: 2,
-    intervals: 'repetition'
+    intervals: 'articles'
   },
   {
     code: 'm3',
     talent: 'Mind',
-    terms: 'ut aliquam iaculis, lacus pede sagittis augue, eu tempor erat',
+    terms: 'Learn something new and read ',
     count: 3,
-    intervals: 'chapters'
+    intervals: 'article'
   },
   {
     code: 'm4',
     talent: 'Mind',
-    terms: 'Vivamus molestie dapibus ligula. Aliquam erat volutpat. Nulla dignissim. Maecenas',
+    terms: 'Google a riddle and try to solve it in ',
     count: 4,
-    intervals: 'pages'
+    intervals: 'minutes'
   },
   {
     code: 'm5',
     talent: 'Mind',
-    terms: 'amet metus. Aliquam erat volutpat. Nulla facilisis. Suspendisse commodo tincidunt',
+    terms: 'Read a non-fiction book for ',
     count: 5,
-    intervals: 'counts'
+    intervals: 'minutes'
   },
   {
     code: 'm6',
     talent: 'Mind',
-    terms: 'vel lectus. Cum sociis natoque penatibus et magnis dis parturient',
+    terms: 'Read a non-fiction book for',
     count: 6,
-    intervals: 'counts'
+    intervals: 'minutes'
   },
   {
     code: 'm7',
     talent: 'Mind',
-    terms: 'augue id ante dictum cursus. Nunc mauris elit, dictum eu,',
+    terms: 'Learn something new and read articles for ',
     count: 7,
-    intervals: 'pages'
+    intervals: 'minutes'
   },
   {
     code: 'm8',
     talent: 'Mind',
-    terms: 'luctus aliquet odio. Etiam ligula tortor, dictum eu, placerat eget,',
+    terms: 'Teach something to someone for',
     count: 8,
     intervals: 'minutes'
   },
   {
     code: 'm9',
     talent: 'Mind',
-    terms: 'fermentum vel, mauris. Integer sem elit, pharetra ut, pharetra sed,',
+    terms: 'Have someone teach you something for',
     count: 9,
     intervals: 'minutes'
   },
   {
     code: 'm10',
     talent: 'Mind',
-    terms: 'Etiam imperdiet dictum magna. Ut tincidunt orci quis lectus. Nullam',
+    terms: 'Read a non-fiction book for',
     count: 10,
-    intervals: 'repetition'
+    intervals: 'minutes'
   },
   {
     code: 'm11',
     talent: 'Mind',
-    terms: 'mi lorem, vehicula et, rutrum eu, ultrices sit amet, risus.',
+    terms: 'Read a non-fiction book for',
     count: 11,
     intervals: 'minutes'
   },
   {
     code: 'm12',
     talent: 'Mind',
-    terms: 'enim non nisi. Aenean eget metus. In nec orci. Donec',
+    terms: 'Read either 1 or',
     count: 12,
     intervals: 'chapters'
   },
   {
     code: 'm13',
     talent: 'Mind',
-    terms: 'per inceptos hymenaeos. Mauris ut quam vel sapien imperdiet ornare.',
+    terms: 'Read 3 articles or read for',
     count: 13,
-    intervals: 'repetition'
+    intervals: 'minutes'
   },
-]
+ ]
+ 
+ 
