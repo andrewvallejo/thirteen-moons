9b8e294d375bff1690fd400a3deaec6265e012d8
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-09 19:32:29 -0600

    Update moon dialog for each level

--- src/components/MoonMsgBar.js
+++ src/components/MoonMsgBar.js
@@ -15,7 +15,6 @@ export const MoonMsgBar = () => {
                 <Route  path='/wrong-turn'>
                   <h1 className= 'dialog'>Where do you think you are?</h1>
                 </Route>
-                
                 <Route exact path='/lunares/'>
                   <h1 className='dialog'>Choose your fate, child</h1>
                 </Route>
@@ -24,6 +23,39 @@ export const MoonMsgBar = () => {
                 </Route>
                 <Route exact path='/lunares/2'>
                   <h1 className='dialog'>Good, keep on going.</h1>
+                </Route>
+                <Route exact path='/lunares/3'>
+                  <h1 className='dialog'>You have three. Remember nothing is for free</h1>
+                </Route>
+                <Route exact path='/lunares/4'>
+                  <h1 className='dialog'>I can tell you're amazing from up here</h1>
+                </Route>
+                <Route exact path='/lunares/5'>
+                  <h1 className='dialog'>Amazing, keep on going.</h1>
+                </Route>
+                <Route exact path='/lunares/6'>
+                  <h1 className='dialog'>You're almost half way there</h1>
+                </Route>
+                <Route exact path='/lunares/7'>
+                  <h1 className='dialog'>And now you're closer to the end than you are to the start</h1>
+                </Route>
+                <Route exact path='/lunares/8'>
+                  <h1 className='dialog'>I count 8 moons, child</h1>
+                </Route>
+                <Route exact path='/lunares/9'>
+                  <h1 className='dialog'>I can see the night getting bright</h1>
+                </Route>
+                <Route exact path='/lunares/10'>
+                  <h1 className='dialog'>Excellent work, child</h1>
+                </Route>
+                <Route exact path='/lunares/11'>
+                  <h1 className='dialog'>See how far curiousity and faith has brought you?</h1>
+                </Route>
+                <Route exact path='/lunares/12'>
+                  <h1 className='dialog'>You don't fear anything, do you?</h1>
+                </Route>
+                <Route exact path='/lunares/13'>
+                  <h1 className='dialog'>You've completed a full circuit child</h1>
                 </Route>
                 <Route exact path='/'>
                   <h1 className='dialog'>Do you wish to craft a Moon challenge card?</h1>
