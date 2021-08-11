ff3f0b0b8ec0b804f2d9049be5839591b20b867f
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-11 10:26:04 -0600

    Add component to hold the all cards view

---
+++ src/components/Collection.js
@@ -0,0 +1,21 @@
+import React from "react"
+import PropTypes from 'prop-types';
+import { MoonCard } from './MoonCard'
+import { MoonMsgBar } from './MoonMsgBar'
+
+export const Collection = ({ deck }) => {
+  return (
+      <section className="card-collection">
+        <MoonMsgBar />
+        <div className="card-containers">
+          <MoonCard cards={deck} />
+        </div>
+      </section>
+  )
+}
+
+Collection.propTypes = {
+  deck: PropTypes.array,
+  hand: PropTypes.array,
+  draw: PropTypes.func
+}
