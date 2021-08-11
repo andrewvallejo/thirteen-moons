1dcf78f8ea8fade8415e85acb6454a4a701d07b7
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-11 10:58:57 -0600

    Add back to home button

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -2,6 +2,7 @@ import React from "react"
 import PropTypes from 'prop-types';
 import { MoonCard } from './MoonCard'
 import { MoonMsgBar } from './MoonMsgBar'
+import { Link } from "react-router-dom";
 
 export const Collection = ({ deck }) => {
   return (
@@ -10,6 +11,8 @@ export const Collection = ({ deck }) => {
         <div className="card-containers">
         <MoonCard cards={deck} />
         </div>
+        <Link className="back-button" to="/">Go Back</Link>
+
       </section>
   )
 }
