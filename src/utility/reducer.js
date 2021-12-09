0f889d1fa02fed4a2ae7b59d2f072b0d7f9a9a81
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-12-08 18:07:22 -0700

    Add reducer for creation form

--- src/utility/reducer.js
+++ src/utility/reducer.js
@@ -4,12 +4,26 @@ export const reducer = (state, { action }) => {
     case 'ADD_DECK':
       return { ...state, deck: value }
     case 'UPDATE_DECK':
-      return { ...state, deck: action.deck }
+      return { ...state, deck: value }
     case 'UPDATE_HAND':
-      return { ...state, hand: action.deck }
-    case 'ADD_ERROR':
-      return { ...state, hand: action.status }
+      return { ...state, hand: value }
     default:
       return state
   }
 }
+
+export const formReducer = (state, {action}) => {
+  const {type, value} = action
+  switch (type) {
+    case 'UPDATE_TALENT':     
+      return { ...state, talent: value }
+    case 'UPDATE_TERMS':     
+      break;
+    case 'UPDATE_COUNT':     
+      break;
+    case 'UPDATE_INTERVALS':     
+      break;
+    default:
+      return state;
+  }
+} 
