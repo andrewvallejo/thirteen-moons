13036653ba49d68e0e9660c741bc5cc20cf52b31
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-12-09 17:22:17 -0700

    Add draw effect logic in useEffect

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -1,15 +1,15 @@
-import { useContext } from "react"
-import { Spread } from "../components/Spread"
-import { GameContext } from "../utility/GameContext"
-import { drawHand } from "../utility/util"
-
+import { useContext, useEffect } from 'react'
+import { Spread } from '../components/Spread'
+import { GameContext } from '../utility/GameContext'
+import { drawHand } from '../utility/util'
 
 export const GamePage = () => {
   const { state, dispatch } = useContext(GameContext)
 
-  return (
-    <>
-        {<Spread deck={state.deck} hand={state.hand}/> }
-    </>
-  )
+  useEffect(() => {
+    const hand = drawHand(state.deck)
+    dispatch({state, action:{type:'UPDATE_HAND', value: hand}})
+  }, [dispatch, state])
+
+  return (<>{<Spread deck={state.deck} hand={state.hand} />}</>)
 }
