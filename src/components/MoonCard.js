d504198b854f77f703eee9ea741b50d732c16d40
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-12-23 11:33:45 -0700

    Simplify moon card to be more generic

--- src/components/MoonCard.js
+++ src/components/MoonCard.js
@@ -1,52 +1,31 @@
-import { nanoid } from 'nanoid'
+import { useContext } from 'react'
+import { GameContext } from '../utility/GameContext'
+import { randomCover } from '../utility/util'
 
-import React, { useState } from 'react'
-import { Link, useRouteMatch } from 'react-router-dom'
+export const MoonCard = () => {
+	const { state: { creationCard } } = useContext(GameContext)
+	const { code, talent, terms, count, intervals } = creationCard
 
-import { createCover } from '../utility/util'
-
-export const MoonCard = ({ cards, draw }) => {
-
-  const [level, setLevel] = useState(1)
-  const match = useRouteMatch().url
-  return cards.map((card) => {
-    const id = nanoid(10)
-    const { code, talent, terms, count, intervals } = card
-    const endTurn = () => {
-      setLevel(level + 1)
-      draw()
-    }
-
-    return (
-      <article className="moon-card" key={id}>
-        <Link
-          to={match.includes('lunares') ? `${match}/${level}` : match}
-          onClick={() => {
-            if (match.includes('lunares')) endTurn()
-          }}>
-          <div className="inner-card">
-            <div className="card-front">
-              <aside className="card-content">
-                <h2 className="talent">{talent}</h2>
-                <h3 className="terms"> {terms}</h3>
-                <h3 className="count">{count}</h3>
-                <h4 className="intervals">{intervals}</h4>
-                <h3 className="talent mirrored">{talent}</h3>
-              </aside>
-            </div>
-            <img
-              className="card-back"
-              id={code}
-              key={id}
-              alt="card"
-              src={createCover()}
-            />
-            <aside className="darken" />
-            <aside className="overlay" />
-          </div>
-        </Link>
-      </article>
-    )
-  })
+	return (
+		<article className='moon-card' key={code} id={code}>
+			<section className='inner-card'>
+				<img className='card-back' alt='card' src={randomCover} />
+				<div className='darken' />
+				<div className='overlay' />
+				{/*  */}
+				<section className='card-front'>
+					<div className='card-content'>
+						<h2 className='talent'>{talent}</h2>
+						<h3 className='terms'>
+							{terms}
+							<span className='count'>{count}</span>
+							{intervals}
+						</h3>
+						<h3 className='talent mirrored'>{talent}</h3>
+					</div>
+				</section>
+				{/*  */}
+			</section>
+		</article>
+	)
 }
-
