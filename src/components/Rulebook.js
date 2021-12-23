9d6b54406ae8577113f2c12ed780c1f99dcc8526
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-12-23 11:34:13 -0700

    Seperate rules into different components

---
+++ src/components/Rulebook.js
@@ -0,0 +1,20 @@
+import { Rules } from "./Rules"
+
+export const Rulebook = () => {
+
+
+  return (
+    <article className="rulebook">
+      <ul className="rules">
+        <h2>Talents</h2>
+        <li className="talent">
+          <Rules />
+        </li>
+      </ul>
+    </article>
+  )
+}
+
+
+
+
