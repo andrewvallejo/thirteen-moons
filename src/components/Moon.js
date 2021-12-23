78e0a267af9c347a5863094d8098a373c30de412
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-12-23 11:31:59 -0700

    Move or delete files

--- src/components/Moon.js
+++
@@ -1,9 +0,0 @@
-import { nanoid } from 'nanoid'
-import React from 'react'
-
-import moon from '../assets/moon.png'
-
-export const Moon = () => {
-  const id = nanoid(10)
-  return <img alt="moon" src={moon} key={id} className="moon" />
-}
