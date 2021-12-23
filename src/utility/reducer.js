ac3a8eeef6a4c96d47f9722dcc34eace96374766
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-12-23 11:32:52 -0700

    Combine reducers for simplicity

--- src/utility/reducer.js
+++ src/utility/reducer.js
@@ -1,24 +1,17 @@
 export const reducer = (state, { action }) => {
-  const { type, value } = action
-  switch (type) {
-    case 'ADD_DECK':
-      return { ...state, deck: value }
-    case 'UPDATE_DECK':
-      return { ...state, deck: value }
-    case 'UPDATE_HAND':
-      return { ...state, hand: value }
-    default:
-      return state
-  }
-}
+	console.log('action', action)
+	const { type, value } = action
+	switch (type) {
+		case 'ADD_DECK':
+			return { ...state, deck: value }
+		case 'UPDATE_DECK':
+			return { ...state, deck: value }
+		case 'UPDATE_HAND':
+			return { ...state, hand: value }
+		case 'CREATE_CARD':
+			return { ...state, creationCard: value }
 
-export const formReducer = (state, { action }) => {
-  const { type, value, id } = action
-  switch (type) {
-    case 'UPDATE_CARD':
-      return { ...state, [id]: value }
-
-    default:
-      return state
-  }
+		default:
+			return state
+	}
 }
