7958d21273294c6833d2887449e0a932cd48e132
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-12-23 11:33:04 -0700

    Remove dummy cover

--- src/utility/cardCovers.js
+++ src/utility/cardCovers.js
@@ -10,18 +10,4 @@ import cover9 from '../assets/saturn-pandelum.png'
 import cover10 from '../assets/shiny-gem.png'
 import cover11 from '../assets/sun-lunar-flower.png'
 
-export const covers = [
-  cover1,
-  cover2,
-  cover3,
-  cover4,
-  cover5,
-  cover6,
-  cover7,
-  cover8,
-  cover9,
-  cover10,
-  cover11
-]
-
-export const dummyCover = cover8
+export const covers = [ cover1, cover2, cover3, cover4, cover5, cover6, cover7, cover8, cover9, cover10, cover11 ]
