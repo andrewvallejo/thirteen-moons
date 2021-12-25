1f39f8f9837bf9ff7edcb2ebe442299592960e65
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-12-24 17:38:05 -0700

    Remove all unnecessary JSX

--- src/components/MoonCard.js
+++ src/components/MoonCard.js
@@ -8,24 +8,14 @@ export const MoonCard = () => {
 
 	return (
 		<article className='moon-card' key={code} id={code}>
-			<section className='inner-card'>
-				<img className='card-back' alt='card' src={randomCover} />
-				<div className='darken' />
-				<div className='overlay' />
-				{/*  */}
-				<section className='card-front'>
-					<div className='card-content'>
-						<h2 className='talent'>{talent}</h2>
-						<h3 className='terms'>
-							{terms}
-							<span className='count'>{count}</span>
-							{intervals}
-						</h3>
-						<h3 className='talent mirrored'>{talent}</h3>
-					</div>
-				</section>
-				{/*  */}
-			</section>
+			<img className='card-image' alt='card' src={randomCover} />
+			<ul className='card-content'>
+				<li className='talent'>{talent}</li>
+				<li className='terms'>{terms} </li>
+				<li className='count'>{count}</li>
+				<li className='terms'>{intervals}</li>
+				<li className='talent mirrored'>{talent}</li>
+			</ul>
 		</article>
 	)
 }
