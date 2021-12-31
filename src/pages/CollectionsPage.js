70cc8f378248d4bbdc53eea126c2a2fb0ad582c1
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-12-31 10:03:28 -0700

    Update collection page to be a live placeholder

--- src/pages/CollectionsPage.js
+++ src/pages/CollectionsPage.js
@@ -1,11 +1,12 @@
-import { Collection } from '../components/Collection'
+import { MoonMsgBar } from '../components/MoonMsgBar'
 
 export const CollectionPage = () => {
-
-
-  return (
-    <>
-      <Collection />
-    </>
-  )
+	const quote = 'Choose your destiny, child'
+	return (
+		<main className='home-page'>
+			<header className='home-header'>
+				<MoonMsgBar quote={quote} />
+			</header>
+		</main>
+	)
 }
