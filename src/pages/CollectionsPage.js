4ee4301029135f36a40d0c48accf63dab71f6fbe
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-12-31 10:42:39 -0700

    Fix class namne

--- src/pages/CollectionsPage.js
+++ src/pages/CollectionsPage.js
@@ -3,7 +3,7 @@ import { MoonMsgBar } from '../components/MoonMsgBar'
 export const CollectionPage = () => {
 	const quote = 'Choose your destiny, child'
 	return (
-		<main className='home-page'>
+		<main className='collections-page'>
 			<header className='home-header'>
 				<MoonMsgBar quote={quote} />
 			</header>
