ea4ec24ea3835a2502b0f4d7bb3f6b1833b50dc2
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-12-31 10:02:22 -0700

    Remove useEffect logic enitrely into GamePage

--- src/App.js
+++ src/App.js
@@ -1,40 +1,29 @@
-import React, { useEffect, useReducer } from 'react'
+import React, { useReducer } from 'react'
 import { Route } from 'react-router-dom'
 
-// import { CollectionPage } from './pages/CollectionsPage'
+import { CollectionPage } from './pages/CollectionsPage'
 import { CreationPage } from './pages/CreationPage'
-// import { GamePage } from './pages/GamePage'
-import { cleanCards, fetchDeck } from './utility/api'
+import { GamePage } from './pages/GamePage'
+import { HomePage } from './pages/HomePage'
 import { GameContext } from './utility/GameContext'
 import { reducer } from './utility/reducer'
 
 const initialState = {
 	deck: [],
 	hand: [],
-	creationCard: []
+	creationCard: [],
+	gameStarted: false
 }
 
 export const App = () => {
 	const [ state, dispatch ] = useReducer(reducer, initialState)
 
-	useEffect(
-		() => {
-			!state.deck.length &&
-				(async () => {
-					await fetchDeck().then((cards) => {
-						let cleanedCards = cleanCards(cards)
-						dispatch({ state, action: { type: 'ADD_DECK', value: cleanedCards } })
-					})
-				})()
-		},
-		[ state ]
-	)
-
 	return (
 		<GameContext.Provider value={{ state, dispatch }}>
-			{/* <Route path="/lunares/" component={GamePage} /> */}
-			<Route exact path='/' component={CreationPage} />
-			{/* <Route exact path="/collection/" component={CollectionPage} /> */}
+			<Route path='/lunares/' component={GamePage} />
+			<Route path='/creation/' component={CreationPage} />
+			<Route exact path='/' component={HomePage} />
+			<Route exact path='/collection/' component={CollectionPage} />
 		</GameContext.Provider>
 	)
 }
