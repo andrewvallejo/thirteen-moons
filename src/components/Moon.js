580f44e385e055624bb5eb491c10164e773ba4b0
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-01-01 13:07:38 -0700

    Add a complete state for moon based on URL

--- src/components/Moon.js
+++ src/components/Moon.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import moon from '../assets/moon.png'
+import { useState } from 'react'
+import { useLocation } from 'react-router-dom'
 
-export const Moon = () => {
-	return <img alt='moon' src={moon} key={'moon'} className='moon' />
+export const Moon = ({ id }) => {
+	const [ isInactive, setInactive ] = useState(false)
+	const location = useLocation().pathname.split('/')[2]
+
+	useEffect(
+		() => {
+			if (location <= id && !isInactive) {
+				setInactive(true)
+			}
+		},
+		[ id, isInactive, location ]
+	)
+
+	return <img alt='moon' src={moon} key={'moon'} className={`moon ${isInactive && 'inactive'}`} />
 }
