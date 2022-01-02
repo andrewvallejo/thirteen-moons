b9947fd362949cb88fca658d962392636d57a87e
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-01-02 09:27:39 -0700

    Add all of App logic into Index

--- src/index.js
+++ src/index.js
@@ -1,16 +1,43 @@
 import './styles/styles.scss'
 
-import React from 'react'
+import React, { useReducer } from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-import { App } from './App'
+import { CollectionPage } from './pages/CollectionsPage'
+import { CreationPage } from './pages/CreationPage'
+import { GamePage } from './pages/GamePage'
+import { HomePage } from './pages/HomePage'
+import { GameContext } from './store/GameContext'
+import { reducer } from './store/reducer'
 
-ReactDOM.render(
-  <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Router>,
-  document.getElementById('root')
-)
+const initialState = {
+	deck: [],
+	hand: [],
+	creationCard: [],
+	gameStarted: false,
+	level: 1
+}
+
+const App = () => {
+	const [ state, dispatch ] = useReducer(reducer, initialState)
+
+	return (
+		<Router>
+			<React.StrictMode>
+				<GameContext.Provider value={{ state, dispatch }}>
+					<Routes>
+						<Route path='/lunares/' element={<GamePage />}>
+							<Route path=':level' element={<GamePage />} />
+						</Route>
+						<Route path='/creation/' element={<CreationPage />} />
+						<Route exact path='/' element={<HomePage />} />
+						<Route exact path='/collection/' element={<CollectionPage />} />
+					</Routes>
+				</GameContext.Provider>{' '}
+			</React.StrictMode>
+		</Router>
+	)
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
