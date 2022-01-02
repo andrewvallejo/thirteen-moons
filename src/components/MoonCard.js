db5b0ae481784d808cf6d1818bfdf6b5c49ed49f
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-01-02 12:51:22 -0700

    Fix card to have both a front and back side

--- src/components/MoonCard.js
+++ src/components/MoonCard.js
@@ -2,9 +2,11 @@ import { useContext, useState } from 'react'
 import { GameContext } from '../store/GameContext'
 import { randomCover } from '../utility/util'
 
-export const MoonCard = () => {
+export const MoonCard = ({ card }) => {
 	const { state: { creationCard, gameStarted } } = useContext(GameContext)
+
 	const { code, talent, terms, count, intervals } = creationCard
+
 	const [ isFlipped, setIsFlipped ] = useState(false)
 
 	const handleFlip = () => {
@@ -16,17 +18,28 @@ export const MoonCard = () => {
 	}
 
 	return (
-		<article className={`moon-card ${isFlipped && 'flipped'}`} key={code} id={code} onClick={handleFlip}>
-			<div className='card-contents'>
-				<ul className={`card-traits ${isFlipped && 'flipped'}`}>
-					<li className={`talent ${isFlipped && 'flipped'}`}>{talent}</li>
-					<li className={`terms ${isFlipped && 'flipped'}`}>{terms} </li>
-					<li className={`count ${isFlipped && 'flipped'}`}>{count}</li>
-					<li className={`intervals ${isFlipped && 'flipped'}`}>{intervals}</li>
-					<li className={`talent mirrored ${isFlipped && 'flipped'}`}>{talent}</li>
-				</ul>
+		<article className='container' key={code} id={code} onClick={handleFlip}>
+			<div className={`card ${isFlipped && 'flipped'}`}>
+				<div className='back'>
+					<ul className='card-contents'>
+						<li className='talent'>{talent}</li>
+						<li className='count'>{count}</li>
+						<li className='talent mirrored'>{talent}</li>
+					</ul>
+
+					<img className='card-image' alt='card' src={randomCover} />
+				</div>
+				<div>
+					<ul className='card-contents'>
+						<li className='talent'>{talent}</li>
+						<li className='terms'>{terms} </li>
+						<li className='count'>{count}</li>
+						<li className='intervals'>{intervals}</li>
+						<li className={`talent mirrored ${isFlipped && 'flipped'}`}>{talent}</li>
+					</ul>
+					<img className='card-image' alt='card' src={randomCover} />
+				</div>
 			</div>
-			<img className='card-image' alt='card' src={randomCover} />
 		</article>
 	)
 }
