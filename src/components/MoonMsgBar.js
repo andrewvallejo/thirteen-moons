d24bdd42cfc3ec68b58b372b59bd26583faeb513
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-01-02 13:40:36 -0700

    Refactor Link to button to dispatch game state false

--- src/components/MoonMsgBar.js
+++ src/components/MoonMsgBar.js
@@ -1,15 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import moonFace from '../assets/images/moon-face.png'
+import { GameContext } from '../store/GameContext'
 
 export const MoonMsgBar = ({ quote }) => {
+	const { dispatch } = useContext(GameContext)
+	const navigate = useNavigate()
+
+	const handleClick = () => {
+		dispatch({ type: 'SET_GAME', gameStarted: false })
+		navigate('/')
+	}
 	return (
 		<article className='message-bar' key='moon'>
 			<blockquote className='dialog'>{quote}</blockquote>
-			<Link to='/'>
+			<button onClick={handleClick}>
 				<img className='moon' alt='the face of the moon' src={moonFace} />
-			</Link>
+			</button>
 		</article>
 	)
 }
