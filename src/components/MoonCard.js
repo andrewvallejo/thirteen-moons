1220d1ad5f2b3cd99c341673065a4bd4d0e81a8f
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-01-02 17:58:14 -0700

    Add switch like conditionals for game progression

--- src/components/MoonCard.js
+++ src/components/MoonCard.js
@@ -1,40 +1,58 @@
 import { useContext, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { GameContext } from '../store/GameContext'
 import { randomCover } from '../utility/util'
 
 export const MoonCard = ({ card }) => {
 	const { state: { creationCard, gameStarted } } = useContext(GameContext)
-	const [ isFlipped, setIsFlipped ] = useState(false)
+	const [isFlipped, setFlipped] = useState(false)
+	const [clicks, setClicks] = useState(0)
 
+	const [nextClass, setNextClass] = useState('next hidden')
 	const mooncard = card || creationCard
 
 	const { code, talent, terms, count, intervals } = mooncard
 
+	let location = useLocation().pathname.split('/')[2]
+	const navigate = useNavigate()
+
 	const handleFlip = () => {
-		if (gameStarted && !isFlipped) {
-			setIsFlipped(true)
-		} else {
-			setIsFlipped(false)
+		if (gameStarted && !isFlipped && clicks === 0) {
+			setFlipped(true)
+			setClicks(clicks + 1)
+		}
+		if (clicks === 1) {
+			setClicks(clicks + 1)
+		}
+		if (clicks === 2) {
+			setNextClass('next')
+			setClicks(clicks + 1)
+		}
+		if (clicks >= 3) {
+			setClicks(clicks + 1)
+			location++
+			navigate(`/lunares/${location}`)
 		}
 	}
 
 	return (
 		<article className='card' key={code} id={code} onClick={handleFlip}>
 			<div className={`card-container ${isFlipped && 'flipped'}`}>
-				<div className='back'>
+				<div>
 					<ul className='card-contents'>
 						<li className='talent'>{talent}</li>
 						<li className='count'>{count}</li>
 						<li className='talent mirrored'>{talent}</li>
 					</ul>
-
 					<img className='card-image' alt='card' src={randomCover} />
 				</div>
-				<div>
+				<div className='back'>
 					<ul className='card-contents'>
 						<li className='talent'>{talent}</li>
 						<li className='terms'>{terms} </li>
 						<li className='count'>{count}</li>
+						<li className={nextClass}>Attempt?</li>
 						<li className='intervals'>{intervals}</li>
 						<li className={`talent mirrored ${isFlipped && 'flipped'}`}>{talent}</li>
 					</ul>
