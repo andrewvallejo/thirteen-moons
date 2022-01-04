08f802a5c071824ee51bce885aa2fc971d824546
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-01-04 11:22:58 -0700

    Fix moon param to be empty since unused

--- src/components/MoonLevels.js
+++ src/components/MoonLevels.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { Moon } from './Moon'
 
 export const MoonLevels = () => {
-	const moons = Array.from(Array(13), (moon, level) => <Moon id={level} key={level} />)
+	const moons = Array.from(Array(13), (_, level) => <Moon id={level + 1} key={level} />)
 
 	return <article className='moon-levels'>{moons}</article>
 }
