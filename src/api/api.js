751c5dbd82e03e779a76491721d35a169171f959
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-01-10 09:20:11 -0700

    Remove old API link and add my server

--- src/api/api.js
+++ src/api/api.js
@@ -1,52 +1,17 @@
-import { cardData } from "../assets/data/cardData"
-
-const deckUrl = 'https://deckofcardsapi.com/api/deck/new/'
-const drawUrl = (id, remaining) => `https://deckofcardsapi.com/api/deck/${id}/draw/?count=${remaining}`
+const axios = require('axios')
 
 export const fetchDeck = async () => {
-	const { deck_id, remaining } = await fetchApi(deckUrl)
-	const cards = fetchApi(drawUrl(deck_id, remaining)).then(({ cards }) => {
-		return cleanCards(cards)
-	})
-	return cards
-}
-
-const fetchApi = async (url) => {
-	return await fetch(url).then((response) => response.json()).catch((error) => console.error('Can not reach server.'))
-}
-
-const cleanCards = (cards) => {
-	return cards.map((card) => {
-		let { value, suit } = card
-
-		if (value === 'ACE') value = 1
-		if (value === 'JACK') value = 11
-		if (value === 'QUEEN') value = 12
-		if (value === 'KING') value = 13
-
-		if (suit === 'SPADES') suit = 'vitality'
-
-		if (suit === 'DIAMONDS') suit = 'spirit'
-		if (suit === 'CLUBS') suit = 'wisdom'
-		if (suit === 'HEARTS') suit = 'mind'
-
-		const newCard = {
-			code: suit[0] + value,
-			talent: suit,
-			count: parseInt(value)
+	let config = {
+		method: 'get',
+		url: 'http://moon-cards-api.vercel.app/cards',
+		headers: {
+			'Content-Type': 'application/json'
 		}
-
-		updateCardInfo(newCard)
-		return newCard
-	})
-}
-const updateCardInfo = (card) => {
-	return cardData.map((data) => {
-		if (data.code === card.code) {
-			card.terms = data.terms
-			card.intervals = data.intervals
-		}
-		return card
-	})
-
+	}
+	try {
+		const response = await axios(config)
+		return response.data
+	} catch (error) {
+		console.log(error)
+	}
 }
